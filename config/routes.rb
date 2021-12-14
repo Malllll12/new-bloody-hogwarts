6fd2d1a994d2f0c991fd10393cce80c8c7993c9b
Mallory Vining <mallorygallery12@gmail.com>
2021-12-13 19:24:15 -0700

    User Story 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+
+  # get "/students/:id", to: "students#index"
+  get "/students", to: "students#index"
 end
---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Student Index' do
+#   As a visitor,
+# When I visit '/students',
+# I see a list of students with the following information:
+# -Name
+# -Age
+# -House
+# (e.g. "Name: Casseopia Black, Age: 14, House: Slytherin")
+
+  it "shows a list of students and their info" do
+    casseopia = Student.create!(name: "Casseopia Black", age: 14, house: "Slytherin")
+    hermione = Student.create!(name: "Hermione Granger", age: 15, house: "Gryffindor")
+
+    visit "/students"
+
+    within "#student-#{hermione.id}" do
+      expect(page). to have_content(hermione.name)
+      expect(page). to have_content(hermione.age)
+      expect(page). to have_content(hermione.house)
+    end
+  end
+end
